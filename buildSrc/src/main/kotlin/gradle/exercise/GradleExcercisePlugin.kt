186cb8f47e3f0f8ee7399ece142e1de1d99d2cc0
Paul Lysak <paul.lysak@gmail.com>
2019-09-10 18:13:35 +0300

    Write down current exercise

--- buildSrc/src/main/kotlin/gradle/exercise/GradleExcercisePlugin.kt
+++ buildSrc/src/main/kotlin/gradle/exercise/GradleExcercisePlugin.kt
@@ -10,12 +10,14 @@ class GradleExcercisePlugin : Plugin<Project> {
 
     private val exerciseFilePattern = Regex("^exercise_[0-9][0-9][0-9]_\\w+\$")
 
+    private val currentExerciseFile = ".current_exercise"
+
 
     override fun apply(project: Project) {
 
-        fun exerciseNames(): List<String> {
+        val exerciseNames: List<String> by lazy {
             val curriculumFiles = project.file(curricullumFolder).listFiles().toList()
-            return curriculumFiles.mapNotNull { file ->
+            curriculumFiles.mapNotNull { file ->
                 if (file.nameWithoutExtension.matches(exerciseFilePattern) && (file.isDirectory || file.extension.toLowerCase() == "zip"))
                     file.nameWithoutExtension
                 else
@@ -23,6 +25,18 @@ class GradleExcercisePlugin : Plugin<Project> {
             }.sorted()
         }
 
+        val exerciseNamesIndexed by lazy { exerciseNames.withIndex() }
+
+        fun currentExName(): String? {
+            return project.file(currentExerciseFile).readText()
+        }
+
+        fun switchExercise(targetExercise: String) {
+            println("TODO: swtich to $targetExercise")
+            //TODO swap test dirs content
+
+            project.file(currentExerciseFile).writeText(targetExercise)
+        }
 
         project.task("man") {
             doLast {
@@ -34,8 +48,50 @@ class GradleExcercisePlugin : Plugin<Project> {
         project.task("listEx") {
             doLast {
                 println("All available exercises:")
-                for ((i, name) in exerciseNames().withIndex()) {
+                for ((i, name) in exerciseNames.withIndex()) {
                     println("${i + 1}. $name")
+                }
+            }
+        }
+
+        project.task("nextEx") {
+            doLast {
+                val allExcercises = exerciseNames
+                val currentEx = currentExName()
+                val currentIndex: Int = currentEx?.let { allExcercises.indexOf(it) } ?: -1
+                val firstEx = allExcercises.first()
+                if (currentEx == null || currentIndex < 0) {
+                    println("Current exercise not known, navigating to the first ${firstEx}")
+                    switchExercise(firstEx)
+                } else if (currentIndex == allExcercises.size - 1) {
+                    println("Already at the last exercise $currentEx, not switching")
+                } else {
+                    switchExercise(allExcercises.get(currentIndex + 1))
+                }
+            }
+        }
+
+        project.task("prevEx") {
+            doLast {
+                val allExcercises = exerciseNames
+                val currentEx = currentExName()
+                val currentIndex: Int = currentEx?.let { allExcercises.indexOf(it) } ?: -1
+                val firstEx = allExcercises.first()
+                if (currentEx == null || currentIndex < 0) {
+                    println("Current exercise not known, navigating to the first ${firstEx}")
+                    switchExercise(firstEx)
+                } else if (currentIndex == 0) {
+                    println("Already at the first exercise $currentEx, not switching")
+                } else {
+                    switchExercise(allExcercises.get(currentIndex - 1))
+                }
+            }
+        }
+
+        for ((i, name) in exerciseNamesIndexed) {
+            project.task("gotoEx$i") {
+                doLast {
+                    switchExercise(name)
                 }
             }
         }
